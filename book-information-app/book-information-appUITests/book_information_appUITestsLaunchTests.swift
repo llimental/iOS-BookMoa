c11c35e1428d0fe0af9a98cd3884c3f68db3d4e9
Lust3r <s1skyblue1s@gmail.com>
2023-05-17 13:17:42 +0900

    [Build] 빈 프로젝트 파일 생성

---
+++ book-information-app/book-information-appUITests/book_information_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  book_information_appUITestsLaunchTests.swift
+//  book-information-appUITests
+//
+//  Created by 이상윤 on 2023/05/17.
+//
+
+import XCTest
+
+final class book_information_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
