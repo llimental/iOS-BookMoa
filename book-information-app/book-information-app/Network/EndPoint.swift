00db3add509fdc1171041ea3fd68b0472438e2ac
Lust3r <s1skyblue1s@gmail.com>
2023-05-18 17:11:02 +0900

    [Feat] #2 Network 폴더 및 파일 초기 구현
    - 네트워크 요청을 담당할 NetworkService 초기 구현
    - URL과 그 구성요소를 담을 EndPoint 초기 구현

---
+++ book-information-app/book-information-app/Network/EndPoint.swift
@@ -0,0 +1,12 @@
+//
+//  EndPoint.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/05/18.
+//
+
+import Foundation
+
+final class EndPoint {
+    
+}
---
+++ book-information-app/book-information-app/Network/NetworkService.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkService.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/05/18.
+//
+
+import Foundation
+
+final class NetworkService {
+    
+}
