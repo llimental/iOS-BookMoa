5999f81f9f535cfc7d18ad115cf21ba59b933423
Lust3r <s1skyblue1s@gmail.com>
2023-05-18 13:26:49 +0900

    [Test] #1 BestSeller DTO 모델에 데이터가 잘 파싱되는지 Unit Test 진행
    - MockData의 BestSeller.json 파일 경로가 잘 설정되었는지 체크
    - json 파일의 내용 확인
    - json 파일의 data 인코딩 결과 확인
    - data의 값을 DTO에 디코딩하여 파싱한 결과 확인
    - 파싱한 데이터의 값이 json 파일의 내용과 맞는지 확인

--- book-information-app/book-information-appTests/book_information_appTests.swift
+++ book-information-app/book-information-appTests/book_information_appTests.swift
@@ -11,26 +11,114 @@ import XCTest
 final class book_information_appTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_Bundle_설정값이_nil이_아닌지_확인() {
+        // Given
+        let bundle = Bundle(for: type(of: self))
+
+        // Then
+        XCTAssertNotNil(bundle)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_filePath가_nil이_아닌지_확인() {
+        // Given
+        let bundle = Bundle(for: type(of: self))
+
+        // When
+        guard let filePath = bundle.path(forResource: "BestSeller", ofType: "json") else { return }
+
+        // Then
+        XCTAssertNotNil(filePath)
     }
 
+    func test_MockData가_nil이_아닌지_확인() {
+        // Given
+        let bundle = Bundle(for: type(of: self))
+
+        guard let filePath = bundle.path(forResource: "BestSeller", ofType: "json") else { return }
+
+        // When
+        guard let jsonString = try? String(contentsOfFile: filePath) else { return }
+
+        // Then
+        XCTAssertNotNil(jsonString)
+    }
+
+    func test_jsonString이_data로_변환이_잘_되었는지_확인() {
+        // Given
+        let bundle = Bundle(for: type(of: self))
+
+        guard let filePath = bundle.path(forResource: "BestSeller", ofType: "json") else { return }
+
+        guard let jsonString = try? String(contentsOfFile: filePath) else { return }
+
+        // When
+        guard let data = jsonString.data(using: .utf8) else { return }
+
+        // Then
+        XCTAssertNotNil(data)
+    }
+
+    func test_data를_DTO로_파싱한_결과가_nil이_아닌지_확인() {
+        // Given
+        let bundle = Bundle(for: type(of: self))
+
+        guard let filePath = bundle.path(forResource: "BestSeller", ofType: "json") else { return }
+
+        guard let jsonString = try? String(contentsOfFile: filePath) else { return }
+
+        guard let data = jsonString.data(using: .utf8) else { return }
+
+        // When
+        let decodedData = try? JSONDecoder().decode(BestSeller.self, from: data)
+
+        // Then
+        XCTAssertNotNil(decodedData)
+    }
+
+    func test_decodedData의_publishDate가_맞게_들어왔는지_확인() {
+        // Given
+        let bundle = Bundle(for: type(of: self))
+
+        guard let filePath = bundle.path(forResource: "BestSeller", ofType: "json") else { return }
+
+        guard let jsonString = try? String(contentsOfFile: filePath) else { return }
+
+        guard let data = jsonString.data(using: .utf8) else { return }
+
+        guard let decodedData = try? JSONDecoder().decode(BestSeller.self, from: data) else { return }
+
+        // When
+        let publishDate = decodedData.publishDate
+        let mockPublishDate = "Thu, 18 May 2023 02:45:08 GMT"  // MockData/BestSeller.json의 pubDate 값
+
+        // Then
+        XCTAssertEqual(publishDate, mockPublishDate)
+    }
+
+    func test_decodedData의_item이_맞게_들어왔는지_확인() {
+        // Given
+        let bundle = Bundle(for: type(of: self))
+
+        guard let filePath = bundle.path(forResource: "BestSeller", ofType: "json") else { return }
+
+        guard let jsonString = try? String(contentsOfFile: filePath) else { return }
+
+        guard let data = jsonString.data(using: .utf8) else { return }
+
+        guard let decodedData = try? JSONDecoder().decode(BestSeller.self, from: data) else { return }
+
+        // When
+        guard let firstItem = decodedData.item.first else { return }
+        let mockFirstItemTitle = "여학교의 별 3"  // MockData/BestSeller.json의 첫 번째 Item의 title 값
+
+        // Then
+        XCTAssertEqual(firstItem.title, mockFirstItemTitle)
+    }
 }
