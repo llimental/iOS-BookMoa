05bffeca750eff52593bf3d83defac2c0a0d8ceb
Lust3r <s1skyblue1s@gmail.com>
2023-05-19 13:40:04 +0900

    [Feat] #2 Network Error 핸들링
    - URL을 만들 때 에러처리를 위해 URLComponentsError 파일 생성

---
+++ book-information-app/book-information-app/Network/Error/URLComponentsError.swift
@@ -0,0 +1,12 @@
+//
+//  URLComponentsError.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/05/19.
+//
+
+import Foundation
+
+enum URLComponentsError: Error {
+    case invalidComponent
+}
