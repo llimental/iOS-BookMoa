104cadeec4406419e1bd2671b5dec10ceb3004f9
Lust3r <s1skyblue1s@gmail.com>
2023-05-19 11:35:02 +0900

    [Feat] #2 EndPoint 구현
    - URL을 구성하는 파라미터 선언
    - 다양한 DTO에 사용할 수 있도록 Responsable을 통해 제네릭 타입을 사용하게 구현

--- book-information-app/book-information-app/Network/EndPoint/EndPoint.swift
+++ book-information-app/book-information-app/Network/EndPoint/EndPoint.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
-final class EndPoint {
-    
+final class EndPoint<R>: Responsable {
+    typealias Response = R
+
+    var methodType: HTTPMethodType
+    var baseURL: String
+    var requestType: String
+    var keyParameter: String
+    var keyValue: String
+    var requiredParameter: String
+    var requiredValue: String
+    var firstCommonParameter: String?
+    var secondCommonParameter: String?
+
+    init(methodType: HTTPMethodType,
+         baseURL: String,
+         requestType: String,
+         keyParameter: String,
+         keyValue: String,
+         requiredParameter: String,
+         requiredValue: String,
+         firstCommonParameter: String? = nil,
+         secondCommonParameter: String? = nil) {
+        self.methodType = methodType
+        self.baseURL = baseURL
+        self.requestType = requestType
+        self.keyParameter = keyParameter
+        self.keyValue = keyValue
+        self.requiredParameter = requiredParameter
+        self.requiredValue = requiredValue
+        self.firstCommonParameter = firstCommonParameter
+        self.secondCommonParameter = secondCommonParameter
+    }
 }
