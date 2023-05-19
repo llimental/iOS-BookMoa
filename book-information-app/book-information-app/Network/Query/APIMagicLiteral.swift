62911200c97be5ad3b8b23d9a24dc3627970ab21
Lust3r <s1skyblue1s@gmail.com>
2023-05-19 10:52:32 +0900

    [File] #2 사용하지 않는 파일 제거 및 파라미터 파일 추가
    - APIMagicLiteral 파일 제거
    - 다른 API에서도 사용되는 공통 파라미터를 담는 CommonParameters 파일 추가

--- book-information-app/book-information-app/Network/Query/APIMagicLiteral.swift
+++
@@ -1,8 +0,0 @@
-//
-//  APIMagicLiteral.swift
-//  book-information-app
-//
-//  Created by 이상윤 on 2023/05/18.
-//
-
-import Foundation
---
+++ book-information-app/book-information-app/Network/Query/CommonParameters.swift
@@ -0,0 +1,13 @@
+//
+//  CommonParameters.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/05/19.
+//
+
+import Foundation
+
+enum CommonParameters {
+    static let maxResults: String = "MaxResults=10"
+    static let searchTarget: String = "SearchTarget=book"
+}
