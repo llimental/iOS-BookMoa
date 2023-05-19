f8bbb989235a44560a1aec59d677019af0ed71c4
Lust3r <s1skyblue1s@gmail.com>
2023-05-19 13:49:38 +0900

    [Feat] #2 네트워크 URL 및 Request 요청 메서드를 갖는 Requestable 프로토콜 구현
    - Requestable 프로토콜의 프로퍼티에 따라 EndPoint에도 반영

--- book-information-app/book-information-app/Network/EndPoint/EndPoint.swift
+++ book-information-app/book-information-app/Network/EndPoint/EndPoint.swift
@@ -7,36 +7,30 @@
 
 import Foundation
 
-final class EndPoint<R>: Responsable {
+final class EndPoint<R>: Requestable, Responsable {
     typealias Response = R
 
     var methodType: HTTPMethodType
     var baseURL: String
     var requestType: String
     var keyParameter: String
-    var keyValue: String
     var requiredParameter: String
-    var requiredValue: String
-    var firstCommonParameter: String?
-    var secondCommonParameter: String?
+    var output: String
+    var apiVersion: String
 
     init(methodType: HTTPMethodType,
          baseURL: String,
          requestType: String,
          keyParameter: String,
-         keyValue: String,
          requiredParameter: String,
-         requiredValue: String,
-         firstCommonParameter: String? = nil,
-         secondCommonParameter: String? = nil) {
+         output: String,
+         apiVersion: String) {
         self.methodType = methodType
         self.baseURL = baseURL
         self.requestType = requestType
         self.keyParameter = keyParameter
-        self.keyValue = keyValue
         self.requiredParameter = requiredParameter
-        self.requiredValue = requiredValue
-        self.firstCommonParameter = firstCommonParameter
-        self.secondCommonParameter = secondCommonParameter
+        self.output = output
+        self.apiVersion = apiVersion
     }
 }
--- book-information-app/book-information-app/Network/EndPoint/Requestable.swift
+++ book-information-app/book-information-app/Network/EndPoint/Requestable.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+protocol Requestable {
+    var methodType: HTTPMethodType { get }
+    var baseURL: String { get }
+    var requestType: String { get }
+    var keyParameter: String { get }
+    var requiredParameter: String { get }
+    var output: String { get }
+    var apiVersion: String { get }
+}
+
+extension Requestable {
+    // MARK: - Public Functions
+
+    func receiveURLRequest() throws -> URLRequest {
+        let url = try receiveURL()
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = HTTPMethodType.get.rawValue
+
+        return urlRequest
+    }
+    
+    // MARK: - Private Functions
+
+    private func receiveURL() throws -> URL {
+        let originalURL = "\(baseURL)\(requestType)"
+
+        guard var components = URLComponents(string: originalURL) else {
+            throw URLComponentsError.invalidComponent
+        }
+
+        let keyQuery = URLQueryItem(name: keyParameter, value: Basic.key)
+        let requiredQuery = URLQueryItem(name: requiredParameter, value: QueryType.bestSeller.rawValue)
+        let outputQuery = URLQueryItem(name: output, value: OutputType.json.rawValue)
+        let versionQuery = URLQueryItem(name: apiVersion, value: APIVersion.versionNumber.rawValue)
+
+        components.queryItems = [keyQuery, requiredQuery, outputQuery, versionQuery]
+
+        guard let changedURL = components.url else {
+            throw URLComponentsError.invalidComponent
+        }
+
+        return changedURL
+    }
+}
