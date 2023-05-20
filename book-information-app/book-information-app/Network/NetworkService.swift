c2eecf8c94fe41d0e1723a60b7215935fe75db46
Lust3r <s1skyblue1s@gmail.com>
2023-05-20 17:09:59 +0900

    [Feat] #2 NetworkService 구현
    - 각 기능별 EndPoint의 URLRequest 메서드를 통해 데이터 요청 작업
    - response에 따른 에러 처리(verifyResponse 메서드)
    - 받은 데이터를 decode 하는 decodeData 메서드
    - 디코딩 된 데이터는 추후 print문 대신 반환값으로 쓰일 예정

--- book-information-app/book-information-app/Network/NetworkService.swift
+++ book-information-app/book-information-app/Network/NetworkService.swift
@@ -9,4 +9,57 @@ import Foundation
 
 final class NetworkService {
 
+    // MARK: - Initialization
+    
+    init() {
+        session = URLSession(configuration: .default)
+    }
+
+    // MARK: - Public Functions
+
+    func receiveBestSellerData() {
+        Task {
+            let networkResult = try await requestData(with: APIEndPoint.receiveBestSeller())
+            print(networkResult)
+        }
+    }
+    
+    // MARK: - Private Functions
+    
+    private func requestData<D: Decodable, R: RequestableAndResponsable>(with endPoint: R) async throws -> D where R.Response == D {
+        let urlRequest = try endPoint.receiveURLRequest()
+
+        let (data, response) = try await session.data(for: urlRequest)
+
+        if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+            try verifyResponse(with: httpResponse)
+        }
+
+        let decodedData: D = try decodeData(with: data)
+
+        return decodedData
+    }
+
+    private func verifyResponse(with httpResponse: HTTPURLResponse) throws {
+        switch httpResponse.statusCode {
+        case (300...399):
+            throw HTTPErrorType.redirectionMessages(httpResponse.statusCode, httpResponse.debugDescription)
+        case (400...499):
+            throw HTTPErrorType.clientErrorResponses(httpResponse.statusCode, httpResponse.debugDescription)
+        case (500...599):
+            throw HTTPErrorType.serverErrorResponses(httpResponse.statusCode, httpResponse.debugDescription)
+        default:
+            throw HTTPErrorType.networkFailError(httpResponse.statusCode, httpResponse.debugDescription)
+        }
+    }
+
+    private func decodeData<D: Decodable>(with requestedData: Data) throws -> D {
+        let decodedData = try JSONDecoder().decode(D.self, from: requestedData) as D
+
+        return decodedData
+    }
+
+    // MARK: - Private Properties
+
+    private let session: URLSession
 }
