5d0e70ee1b9c080942ad3f8aae5d2ebaccd91b10
Lust3r <s1skyblue1s@gmail.com>
2023-05-22 21:31:07 +0900

    [Feat] #3 DiffableDataSource에 사용할 HomeController 구현

--- book-information-app/book-information-app/Presentation/Controllers/HomeController.swift
+++ book-information-app/book-information-app/Presentation/Controllers/HomeController.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 final class HomeController {
-    
+    struct Book: Hashable {
+        let title: String
+        let author: String
+        let identifier = UUID()
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(identifier)
+        }
+    }
+
+    struct BestSeller: Hashable {
+        let title: String
+        let books: [Book]?
+        let identifier = UUID()
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(identifier)
+        }
+    }
 }
