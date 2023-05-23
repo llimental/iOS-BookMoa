862a4a8cb86339ec5b60e28f20d9914c5f280e47
Lust3r <s1skyblue1s@gmail.com>
2023-05-23 18:59:18 +0900

    [Feat] #3 Hashable 준수를 위한 static func 메서드 추가

--- book-information-app/book-information-app/Presentation/Controllers/HomeController.swift
+++ book-information-app/book-information-app/Presentation/Controllers/HomeController.swift
@@ -12,20 +12,27 @@ final class HomeController {
         let title: String
         let author: String
         let cover: UIImage
-        let identifier = UUID()
 
         func hash(into hasher: inout Hasher) {
-            hasher.combine(identifier)
+            hasher.combine(title)
+            hasher.combine(author)
+        }
+
+        static func == (lhs: HomeController.Book, rhs: HomeController.Book) -> Bool {
+            return lhs.title == rhs.title && lhs.author == rhs.author
         }
     }
 
     struct BestSeller: Hashable {
         let section: String
         let books: [Book]?
-        let identifier = UUID()
 
         func hash(into hasher: inout Hasher) {
-            hasher.combine(identifier)
+            hasher.combine(section)
+        }
+
+        static func == (lhs: HomeController.BestSeller, rhs: HomeController.BestSeller) -> Bool {
+            return lhs.section == rhs.section
         }
     }
 }
