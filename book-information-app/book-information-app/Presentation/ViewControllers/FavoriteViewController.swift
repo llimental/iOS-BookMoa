b6cc9a2a33e8db71892d8755194b7e1fc9b5d399
Lust3r <s1skyblue1s@gmail.com>
2023-05-25 21:40:40 +0900

    [Feat] #3 즐겨찾기를 보여줄 FavoriteViewController 추가 및 Deployment Target 변경
    - 프로젝트 Deployment Target을 프로젝트 타겟에 맞춰 15.0으로 변경

---
+++ book-information-app/book-information-app/Presentation/ViewControllers/FavoriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteViewController.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/05/25.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
