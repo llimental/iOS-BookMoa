32882abb50d2a31f3bc672201faa28e89176fdb1
Lust3r <s1skyblue1s@gmail.com>
2023-05-28 21:21:48 +0900

    [Feat] #4 개별 책의 정보를 받기 위한 IndividualBook DTO 구현

---
+++ book-information-app/book-information-app/Data/BookInformationDTO/Response/IndividualBook/IndividualBook.swift
@@ -0,0 +1,44 @@
+//
+//  IndividualBook.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/05/28.
+//
+
+import Foundation
+
+// MARK: - IndividualBook
+struct IndividualBook: Decodable {
+    let item: [Item]
+
+    // MARK: - Item
+    struct Item: Decodable {
+        let title: String
+        let link: String
+        let author: String
+        let pubDate: String
+        let description: String
+        let cover: String
+        let categoryName: String
+        let publisher: String
+        let subInfo: SubInfo
+
+        enum CodingKeys: String, CodingKey {
+            case title
+            case link
+            case author
+            case pubDate
+            case description
+            case cover
+            case categoryName
+            case publisher
+            case subInfo
+        }
+    }
+
+    // MARK: - SubInfo
+    struct SubInfo: Codable {
+        let subTitle, originalTitle: String
+        let itemPage: Int
+    }
+}
