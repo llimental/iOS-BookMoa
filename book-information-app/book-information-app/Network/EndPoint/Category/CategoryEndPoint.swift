2eece194a88d82bee80be7a37a9905e3721abe21
Lust3r <s1skyblue1s@gmail.com>
2023-05-30 22:20:30 +0900

    [Feat] #7 CategoryEndPoint 구현
    - 새로운 EndPoint가 생김에 따라 DTO와 같이 폴더로 구분

---
+++ book-information-app/book-information-app/Network/EndPoint/Category/CategoryEndPoint.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryEndPoint.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/05/30.
+//
+
+import Foundation
+
+final class CategoryEndPoint: RequestableAndResponsable {
+    typealias Response = Category
+
+    var methodType: HTTPMethodType
+    var scheme: String
+    var host: String
+    var path: String
+    var query: [URLQueryItem]
+
+    init(categoryId: String, startIndex: String) {
+        let api = BookAPI.category(categoryId: categoryId, startIndex: startIndex)
+
+        self.methodType = .get
+        self.scheme = BookAPI.scheme
+        self.host = BookAPI.host
+        self.path = api.path
+        self.query = api.query
+    }
+}
