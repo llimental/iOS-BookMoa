4b2279c1f5df5e94c5360c302d6632fb487b1ac5
Lust3r <s1skyblue1s@gmail.com>
2023-05-30 22:15:00 +0900

    [Feat] #7 CategoryDTO 구현

--- book-information-app/book-information-app/Data/BookInformationDTO/Response/Category/Category.swift
+++ book-information-app/book-information-app/Data/BookInformationDTO/Response/Category/Category.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+// MARK: - Category
+struct Category: Decodable {
+    let totalResults: Int
+    let startIndex: Int
+    let itemsPerPage: Int
+    let searchCategoryName: String
+    let item: [Item]
+
+    // MARK: - Item
+    struct Item: Decodable {
+        let title: String
+        let author: String
+        let isbn13: String
+        let cover: String
+    }
+}
