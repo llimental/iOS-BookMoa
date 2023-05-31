9d0ee28d63d5bed9b5b9803fcbcea8e13042785a
Lust3r <s1skyblue1s@gmail.com>
2023-06-01 04:58:15 +0900

    [Feat] #7 SearchViewController 초기 틀 구현

---
+++ book-information-app/book-information-app/Presentation/ViewControllers/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/06/01.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
