79cb85d938b86c51598cfe07dd994122b98c225a
Lust3r <s1skyblue1s@gmail.com>
2023-06-01 04:41:02 +0900

    [Feat] #7 CategoryViewController에 북마크 반영 코드 구현
    - 바로 상태를 반영해야 하기에 viewWillAppear에서 loadData를 반복하는 만큼 부하를 줄이기 위해 animation false 처리
    - loadData 대신 applySnapshot만 해보려 하였으나 적용되지 않음

--- book-information-app/book-information-app/Presentation/ViewControllers/CategoryViewController.swift
+++ book-information-app/book-information-app/Presentation/ViewControllers/CategoryViewController.swift
@@ -16,6 +16,12 @@ final class CategoryViewController: UIViewController {
 
     // MARK: - View LifeCycle
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        loadData()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -70,7 +76,7 @@ final class CategoryViewController: UIViewController {
         snapshot.appendSections([CategoryController.Section.categoryBookList])
         snapshot.appendItems(categoryBookList, toSection: .categoryBookList)
 
-        self.dataSource.apply(snapshot, animatingDifferences: true)
+        self.dataSource.apply(snapshot, animatingDifferences: false)
     }
 
     // MARK: - Private Properties
@@ -157,10 +163,15 @@ extension CategoryViewController {
                 return UICollectionViewCell()
             }
 
+            guard let bookmarkedItems = UserDefaults.standard.stringArray(forKey: MagicLiteral.bookmarkTextForKey) else {
+                return UICollectionViewCell()
+            }
+
             cell.booktitleLabel.text = item.title
             cell.bookAuthorLabel.text = item.author
             cell.bookImageView.image = item.cover
             cell.bookISBN = item.isbn
+            cell.bookmarkImageView.backgroundColor = bookmarkedItems.contains(cell.bookISBN) ? UIColor(red: 0.88, green: 0.04, blue: 0.55, alpha: 1.00) : .white
 
             return cell
         }
