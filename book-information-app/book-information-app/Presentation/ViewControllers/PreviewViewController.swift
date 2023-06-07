fc422d0a7032fc02c05a4c3959e5d7fee76cdae7
Lust3r <s1skyblue1s@gmail.com>
2023-06-08 03:23:55 +0900

    [Feat] #1 PreviewViewController 생성
    - 기존 PopupView 대체 예정

---
+++ book-information-app/book-information-app/Presentation/ViewControllers/PreviewViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PreviewViewController.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/06/07.
+//
+
+import UIKit
+
+final class PreviewViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
