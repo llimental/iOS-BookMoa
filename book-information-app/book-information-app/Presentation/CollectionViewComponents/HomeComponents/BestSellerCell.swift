724c1407aa95930d4199652642ce8ed5d3656a43
Lust3r <s1skyblue1s@gmail.com>
2023-06-09 00:54:24 +0900

    [Fix] BestSellerCell의 제약조건 변경
    - scaleAspectFill로 할 시 가로로 긴 책은 이미지가 겹치기에 scaleAspectFit으로 변경
    - addSubview와 다르게 addArrangedSubview는 trnaslatesAuto 속성을 false로 바꿔줌
    - 때문에 그렇게 추가한 bookImageView의 높이와 너비 제약조건을 추가하여 모호한 크기 레이아웃 이슈 해결

--- book-information-app/book-information-app/Presentation/CollectionViewComponents/HomeComponents/BestSellerCell.swift
+++ book-information-app/book-information-app/Presentation/CollectionViewComponents/HomeComponents/BestSellerCell.swift
@@ -37,25 +37,26 @@ extension BestSellerCell {
         bookStackView.addArrangedSubview(bookAuthorLabel)
 
         bookStackView.axis = .vertical
-        bookStackView.spacing = 3
         bookStackView.alignment = .center
+        bookStackView.distribution = .equalSpacing
 
-        bookImageView.contentMode = .scaleAspectFill
+        bookImageView.contentMode = .scaleAspectFit
 
         booktitleLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
         booktitleLabel.adjustsFontForContentSizeCategory = true
-        booktitleLabel.textAlignment = .center
 
         bookAuthorLabel.font = UIFont.preferredFont(forTextStyle: .caption2)
         bookAuthorLabel.adjustsFontForContentSizeCategory = true
         bookAuthorLabel.textColor = UIColor(red: 0.29, green: 0.31, blue: 0.34, alpha: 1.00)
-        bookAuthorLabel.textAlignment = .center
 
         NSLayoutConstraint.activate([
             bookStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
             bookStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             bookStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             bookStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+
+            bookImageView.heightAnchor.constraint(equalTo: bookStackView.heightAnchor, multiplier: 0.8),
+            bookImageView.widthAnchor.constraint(equalTo: bookStackView.widthAnchor)
         ])
     }
 }
