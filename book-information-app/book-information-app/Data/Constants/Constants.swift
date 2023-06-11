84e7ca1266d3fcdf80f870c6619cb30bdbb26442
Lust3r <s1skyblue1s@gmail.com>
2023-06-12 00:13:26 +0900

    [Feat] #3 앱 주요 색상을 Constants 파일의 enum에 추가

---
+++ book-information-app/book-information-app/Data/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  book-information-app
+//
+//  Created by 이상윤 on 2023/06/12.
+//
+
+import UIKit
+
+enum Constants {
+    static let kMainTintColor: UIColor = UIColor(red: 0.38, green: 0.13, blue: 0.93, alpha: 1.00)
+}
