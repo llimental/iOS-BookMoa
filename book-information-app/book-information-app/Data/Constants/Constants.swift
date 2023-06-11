a6b5b14882ccad859cb0b8d490618b73622c3d80
Lust3r <s1skyblue1s@gmail.com>
2023-06-12 00:21:10 +0900

    [Feat] #3 Constants에 kDividerColor 프로퍼티 추가
    - BookDetailViewController의 divider에 사용할 색상

--- book-information-app/book-information-app/Data/Constants/Constants.swift
+++ book-information-app/book-information-app/Data/Constants/Constants.swift
@@ -9,4 +9,5 @@ import UIKit
 
 enum Constants {
     static let kMainColor: UIColor = UIColor(red: 0.38, green: 0.13, blue: 0.93, alpha: 1.00)
+    static let kDividerColor: UIColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.00)
 }
