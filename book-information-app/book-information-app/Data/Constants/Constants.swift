1471a3e5041af0293bee1528abd39cfdc1eb285e
Lust3r <s1skyblue1s@gmail.com>
2023-06-13 11:57:42 +0900

    [Feat] #3 ConstantsString enum 추가

--- book-information-app/book-information-app/Data/Constants/Constants.swift
+++ book-information-app/book-information-app/Data/Constants/Constants.swift
@@ -11,3 +11,7 @@ enum ConstantsColor {
     static let kMainColor: UIColor = UIColor(red: 0.38, green: 0.13, blue: 0.93, alpha: 1.00)
     static let kDividerColor: UIColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.00)
 }
+
+enum ConstantsString {
+    static let kBlank: String = ""
+}
