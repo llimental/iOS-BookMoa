e70f0ec90fddcd9a5de218786a17cea1271a42e7
Lust3r <s1skyblue1s@gmail.com>
2023-06-14 13:43:37 +0900

    [Feat] #3 favoritesTextForKey 타입 프로퍼티 추가

--- book-information-app/book-information-app/Data/Constants/Constants.swift
+++ book-information-app/book-information-app/Data/Constants/Constants.swift
@@ -45,6 +45,7 @@ enum ConstantsFavorites {
     static let kColor: UIColor = UIColor(red: 0.88, green: 0.04, blue: 0.55, alpha: 1.00)
     static let kButtonImageName: String = "heart.fill"
     static let kCellImageName: String = "heart.square.fill"
+    static let favoritesTextForKey: String = "favorites"
 }
 
 enum ConstantsBookDetailInformation {
